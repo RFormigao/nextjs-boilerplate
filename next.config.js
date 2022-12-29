46b7c1524845f34c1f272b5bb5970b04641ac7ee
Robson Gomes <robson.gomes@agrotis.com>
2022-12-29 16:18:46 -0300

    feature: initialize boilerplate next js with typescript

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+const isProd = process.env.NODE_ENV === 'production'
+
+// const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+  // runtimeCaching
+})
+
+const nextConfig = withPWA({
+  // next config
+})
 
 module.exports = nextConfig
