6be651dc909cf65489f1ea20e44467fa228ab3d1
Robson Gomes <robson.gomes@agrotis.com>
2022-12-29 23:39:57 -0300

    fix: remove config not used

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const isProd = process.env.NODE_ENV === 'production'
 
-// const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: !isProd
-  // runtimeCaching
 })
 
 const nextConfig = withPWA({
-  // next config
+  swcMinify: true
 })
 
 module.exports = nextConfig
